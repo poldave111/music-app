c428e033418f74a397bab9f14f228dfb90ebbb42
unknown <podave111@gmail.com>
2023-10-12 16:25:30 +0200

    Add search dropdwon and input works showing right songs

--- src/js/components/Search.js
+++ src/js/components/Search.js
@@ -9,6 +9,8 @@ class Search {
         thisSearch.getElements();
         thisSearch.render(data);
         thisSearch.initActions(songs);
+        thisSearch.prepareDataForCategories(songs);
+        thisSearch.searchByCategory(songs);
        // thisSearch.prepareDataForCategories(songs);
        //thisSearch.generateSongs(songs);
     }
@@ -22,7 +24,7 @@ class Search {
     initActions(songs) {
         const thisSearch = this; 
         console.log('button', thisSearch.element.querySelector('button'));
-        thisSearch.element.querySelector('button').addEventListener('click', () => {thisSearch.getRightSongs(songs)} );
+        thisSearch.element.querySelector('button').addEventListener('click', () => {thisSearch.searchByTitle(songs)} );
         
     }
 
@@ -31,82 +33,62 @@ class Search {
         thisSearch.element.querySelectorAll('.player').forEach((elem) => elem.classList.add('invisible'));
     }
 
-    getRightSongs(songs) {
+    showSearchResults(filteredSongs) {
+        const thisSearch = this;
+        console.log('filteredSongs', filteredSongs);
+        thisSearch.hideAllPlayers();
+        const players = Array.from(thisSearch.element.querySelectorAll('.player'));
+        console.log('players', players);
+        filteredSongs.forEach((song) => {
+            const player = players.find((player) =>  player.getAttribute('data-src') === song.filename);
+            console.log('player', player);
+            player.classList.remove('invisible');
+        }) 
+    }
+
+    searchByTitle(songs) {
         const thisSearch = this; 
         const searchInput = document.getElementById('searchInput');
         //console.log('searchInput', searchInput);
         const searchValue = searchInput.value.trim();
-        thisSearch.hideAllPlayers();
+        
         thisSearch.filteredSongs = songs.filter(song => {
             const lowerTitle = song.title.toLowerCase();
             const lowerSearchValue = searchValue.toLowerCase();
             return lowerTitle.includes(lowerSearchValue);
         });
-        //console.log('filteredSongs', thisSearch.filteredSongs);
-
-        const players = Array.from(thisSearch.element.querySelectorAll('.player'));
-        console.log('players', players);
-        thisSearch.filteredSongs.forEach((song) => {
-            const player = players.find((player) =>  player.getAttribute('data-src') === song.filename);
-            player.classList.remove('invisible');
-        }) 
-
-        // rightSongs.map((song) => {
-        //     let element = thisSearch.element.querySelector(`source[src="${song}"]`);
-        //     let elem = element.closest('.player.green-audio-player');
-        //     elem.classList.add("invisible");
-        //     // console.log('element', element);
-        //     // console.log('elem', elem);
-        // })
-
-        // console.log(rightSongs);
-        
-        // console.log(players[0].getAttribute("src"));
-        // const containerSongSearch =  document.querySelector('.searchPlayers');
-        // console.log(containerSongSearch);
-        // thisSearch.generateSongs(thisSearch.filteredSongs, containerSongSearch);
-        // from data choose according to search appriopriate data, 
-        // make any change in input field start this method, (eventListener?)
+        console.log('thisSearch.filtered', thisSearch.filteredSongs);
+        thisSearch.showSearchResults(thisSearch.filteredSongs);
     }
 
-    // generateSongs(songs, container) {
-    //     const thisSearch = this;
-    //     console.log('container', container);
-    //     songs.map((song) => {
-    //         const generatedPlayer = templates.player(song)
-    //         let player = utils.createDOMFromHTML(generatedPlayer);
-    //         container.appendChild(player);
-    //     });
-    //     thisSearch.hookPlayers();
-    // }
+    searchByCategory(songs) {
+        const thisSearch = this;
+        const categorySelect = document.getElementById("categorySelect");
+        categorySelect.addEventListener("change", () => {
+            const selectedCategory = categorySelect.value;
+            const filteredSongs = songs.filter(song => song.categories.includes(selectedCategory));
+            thisSearch.showSearchResults(filteredSongs);
+        });
+    }
 
-    // hookPlayers() {
-    //     //console.log('hookplayers ruszyło');
-    //     GreenAudioPlayer.init({
-    //         selector: '.player',
-    //         stopOthersOnPlay: true
-    //     });
-    // }
-
-    // prepareDataForCategories(songs) {
-    //     const thisSearch = this; 
-    //     const uniqueCategories = { categories: [] };
-
-    //     songs.forEach(song => {
-    //         song.categories.forEach(category => {
-    //             if (!uniqueCategories.categories.includes(category)) {
-    //                 uniqueCategories.categories.push(category);
-    //             }
-    //         });
-    //     });
-    //     thisSearch.generateCategories(uniqueCategories);
-    // }
+    prepareDataForCategories(songs) {
+        const thisSearch = this;
+        const uniqueCategories = []; 
+        songs.forEach(song => {
+            song.categories.forEach(category => {
+                if(!uniqueCategories.includes(category)) {
+                    uniqueCategories.push(category);
+                }
+            });
+        });
+        thisSearch.generateCategories(uniqueCategories);
+    }
     generateCategories(categories) {
         const thisSearch = this; 
-        //const container = document.querySelector(select.containerOf.categories);
-        const generatedHTML = templates.categories(categories);
-        thisSearch.element = utils.createDOMFromHTML(generatedHTML);
-        container.appendChild(thisSearch.element);
+        const container = document.querySelector(select.containerOf.categories);
+        const generatedHTML = templates.categories({categories});
+        thisSearch.dropdown = utils.createDOMFromHTML(generatedHTML);
+        container.appendChild(thisSearch.dropdown);
     }
 
     render(data) {
